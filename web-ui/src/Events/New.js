b6b2674c9b2183bb153151b8fb8edd3b6a6dd9f6
kriti-g <kriti7000@gmail.com>
2021-03-24 23:06:23 -0400

    Update New.js

--- web-ui/src/Events/New.js
+++ web-ui/src/Events/New.js
@@ -1,6 +1,6 @@
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import { useState } from 'react'
-import { create_user, fetch_users } from '../api';
+import { create_event } from '../api';
 
 export default function EventsNew() {
   let [eve, setEvent] = useState({});
