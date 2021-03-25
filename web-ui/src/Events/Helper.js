371e4a084dc54a5d310a04a49a2b715744624e45
kriti-g <kriti7000@gmail.com>
2021-03-25 01:35:07 -0400

    Update Helper.js

--- web-ui/src/Events/Helper.js
+++ web-ui/src/Events/Helper.js
@@ -3,6 +3,6 @@ export function inInvites(user_id, eve) {
 }
 
 export function isOwner(user_id, eve) {
-  console.log(["is owner", user_id, eve.user.id])
+  console.log(["is owner", user_id, eve.user.id, user_id === eve.user.id])
   return user_id === eve.user.id;
 }
