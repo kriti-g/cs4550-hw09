f6735d846dd73bc3fbdfa2d73e2af056045c5500
kriti-g <kriti7000@gmail.com>
2021-03-26 00:14:27 -0400

    Update deploy.sh

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,33 +5,33 @@ export MIX_ENV=prod
 # Valid port range for a user app to listen
 # on is something like 1025-32767
 export PORT=4750
-export SECRET_KEY_BASE=insecure
+export SECRET_KEY_BASE="9aZ1EsCPeoHveI1uSkcKBQA2vD6WgadKqDCgRLj1pNiKnL4Fx11/l8YR8GOiWLRW"
 export DATABASE_URL=ecto://user_stories_spa:ainuu2vaeD6i@localhost/user_stories_spa_prod
 
 mix deps.get --only prod
 mix compile
 
-CFGD=$(readlink -f ~/.config/user_stories_spa)
+# CFGD=$(readlink -f ~/.config/user_stories_spa)
+#
+# if [ ! -d "$CFGD" ]; then
+#     mkdir -p "$CFGD"
+# fi
+#
+# if [ ! -e "$CFGD/base" ]; then
+#     mix phx.gen.secret > "$CFGD/base"
+# fi
+#
+# if [ ! -e "$CFGD/db_pass" ]; then
+#     pwgen 12 1 > "$CFGD/db_pass"
+# fi
+#
+# SECRET_KEY_BASE=$(cat "$CFGD/base")
+# export SECRET_KEY_BASE
+#
+# DB_PASS=$(cat "$CFGD/db_pass")
+# export DATABASE_URL=ecto://user_stories:$DB_PASS@localhost/user_stories_spa_prod
 
-if [ ! -d "$CFGD" ]; then
-    mkdir -p "$CFGD"
-fi
-
-if [ ! -e "$CFGD/base" ]; then
-    mix phx.gen.secret > "$CFGD/base"
-fi
-
-if [ ! -e "$CFGD/db_pass" ]; then
-    #pwgen 12 1 > "$CFGD/db_pass"
-fi
-
-SECRET_KEY_BASE=$(cat "$CFGD/base")
-export SECRET_KEY_BASE
-
-DB_PASS=$(cat "$CFGD/db_pass")
-export DATABASE_URL=ecto://user_stories:$DB_PASS@localhost/user_stories_spa_prod
-
-mix ecto.migrate
+mix ecto.reset
 
 mix phx.digest
 
